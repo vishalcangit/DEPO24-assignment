2795d50b4d06fb31a4d296f80314a48e9d2e7dbf
vishalcangit <vishalsuvarna000@gmail.com>
2022-09-06 19:50:35 +0530

    done with responsiveness

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>
 );
